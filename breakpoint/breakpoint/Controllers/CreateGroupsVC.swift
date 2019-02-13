21b6d666bbfc49a6ba32eabe36d750bd67c413f4
Mario Galluscio <mgalluscio@deliverydudes.com>
2019-02-12 23:02:33 -0500

    Set up iboutlets and actions for CreateGroupsVC

--- breakpoint/breakpoint/Controllers/CreateGroupsVC.swift
+++ breakpoint/breakpoint/Controllers/CreateGroupsVC.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class CreateGroupsVC: UIViewController {
-
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailTextField: InsetTextField!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+    }
+    
 }
