89a74f30758d7bc708415fa5c39f6f38f4e73eb3
Mario Galluscio <mgalluscio@deliverydudes.com>
2019-02-13 21:58:10 -0500

    Set up table view cell for CreatGroupsVC. Created UserCell view and testing with three fake, configured cells

--- breakpoint/breakpoint/Controllers/CreateGroupsVC.swift
+++ breakpoint/breakpoint/Controllers/CreateGroupsVC.swift
@@ -18,11 +18,32 @@ class CreateGroupsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     @IBAction func closeBtnWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     @IBAction func doneBtnWasPressed(_ sender: Any) {
+    }
+    
+}
+
+// conform to UiTableView protocols
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: "marty@b2f.com", isSelected: true)
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
 }
