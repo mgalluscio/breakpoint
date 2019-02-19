61586fdad82414daa498c652ea302ea1ffde62f0
Mario Galluscio <mgalluscio@deliverydudes.com>
2019-02-18 21:01:33 -0500

    pulling group messages from db

--- breakpoint/breakpoint/Controllers/GroupFeedVC.swift
+++ breakpoint/breakpoint/Controllers/GroupFeedVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class GroupFeedVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -17,6 +18,7 @@ class GroupFeedVC: UIViewController {
     @IBOutlet weak var sendBtn: UIButton!
     
     var group: Group?
+    var groupMessages = [Message]()
     
     // used to initialize group data from GroupsVC
     func initData(forGroup group: Group) {
@@ -30,16 +32,65 @@ class GroupFeedVC: UIViewController {
         DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
             self.membersLbl.text = returnedEmails.joined(separator: ", ")
         }
+        // observe the REF_GROUPS var so we know when something is changed, updated, removed, etc.
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            // get messages
+            DataService.instance.getAllMessagesFor(desiredGroup: self.group!, handler: { (returnedGroupMessages) in
+                // copy returned messages to array local to class and reload tableView
+                self.groupMessages = returnedGroupMessages
+                self.tableView.reloadData()
+            })
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // binds view to keyboard so it slides up with keyboard
         sendBtnView.bindToKeyboard()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     @IBAction func backButtonWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    // called when send button is pressed
     @IBAction func sendBtnWasPressed(_ sender: Any) {
+        // only send messages that aren't blank
+        if messageTextField.text != "" {
+            // disable textfield and button so no messages can be send twice
+            messageTextField.isEnabled = false
+            sendBtn.isEnabled = false
+            // upload post
+            DataService.instance.uploadPost(withMessage: messageTextField.text!, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: group?.key) { (complete) in
+                // if complete, reenable textfield, button and clear textfield
+                if complete {
+                    self.messageTextField.text = ""
+                    self.messageTextField.isEnabled = true
+                    self.sendBtn.isEnabled = true
+                }
+            }
+        }
     }
+}
+
+extension GroupFeedVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupMessages.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupFeedCell", for: indexPath) as? GroupFeedCell else { return UITableViewCell() }
+        let message = groupMessages[indexPath.row]
+        DataService.instance.getUsername(forUID: message.senderId) { (email) in
+            cell.configureCell(profileImage: UIImage(named: "defaultProfileImage")!, email: email, content: message.content)
+        }
+        return cell
+    }
+    
+    
 }
--- breakpoint/breakpoint/Services/DataService.swift
+++ breakpoint/breakpoint/Services/DataService.swift
@@ -59,7 +59,8 @@ class DataService {
     // uploads posts to db, takes message, user ID and group ID. If group ID exists, send to group ref, else post to feed.
     func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?, sendComplete: @escaping (_ status: Bool) -> ()) {
         if groupKey != nil {
-            // send to groups ref
+            REF_GROUPS.child(groupKey!).child("messages").childByAutoId().updateChildValues(["content":message, "senderId": uid])
+            sendComplete(true)
         } else {
             REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId": uid])
             sendComplete(true)
@@ -100,6 +101,27 @@ class DataService {
                     handler(user.childSnapshot(forPath: "email").value as! String)
                 }
             }
+        }
+    }
+    
+    // function takes a group object as a parameter and returns a message array for that group.
+    func getAllMessagesFor(desiredGroup: Group, handler: @escaping (_ messagesArray: [Message]) -> ()) {
+        // array to hold messages
+        var groupMessageArray = [Message]()
+        // observe single event
+        REF_GROUPS.child(desiredGroup.key).child("messages").observeSingleEvent(of: .value) { (groupMessageSnapshot) in
+            // create snapshot array
+            guard let groupMessageSnapshot = groupMessageSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            // loop over array, pull out content, senderId and create message object
+            for groupMessage in groupMessageSnapshot {
+                let content = groupMessage.childSnapshot(forPath: "content").value as! String
+                let senderId = groupMessage.childSnapshot(forPath: "senderId").value as! String
+                let message = Message(content: content, senderId: senderId)
+                // append message to message array
+                groupMessageArray.append(message)
+            }
+            // return array 
+            handler(groupMessageArray)
         }
     }
     
